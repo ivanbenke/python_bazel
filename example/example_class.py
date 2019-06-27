2c4503c3417cefa69e28d001e2f50f7353038638
ivanbenke <benke.ivan@gmail.com>
2019-06-27 14:22:56 +0200

    rework of class, new unit tests added for trying out testing with Bazel

--- example/example_class.py
+++ example/example_class.py
@@ -1,23 +1,26 @@
 class BazelPythonExample():
-    def __init__(self, person, age):
-        self.person = person
+    def __init__(self, name, age):
+        self.name = name
         self.age = age
     
     def add_X_years(self, X_years):
         self.age += X_years
 
     def print_age(self):
-        if self.person:
-            print "Age: %d" % self.age
-        else:
-            print "Not a person"
+        print "Age: %s" % self.age
+    
+    def print_name(self):
+        print "Name: %s" % self.name
 
-man = BazelPythonExample(1, 56)
+
+man = BazelPythonExample("Mark", 56)
+man.print_name()
 man.print_age()
 man.add_X_years(15)
 man.print_age()
 
-dog = BazelPythonExample(0, 6)
+dog = BazelPythonExample("Rex", 6)
+dog.print_name()
 dog.print_age()
 dog.add_X_years(3)
 dog.print_age()
--- example/example_class_test.py
+++ example/example_class_test.py
@@ -4,16 +4,23 @@ from example_class import BazelPythonExample
 
 class BazelPythonExampleTest(unittest.TestCase):
     def test_addedXyears(self):
-        bpe = BazelPythonExample(1, 15)
-        print "is it not so"
-        self.assertEqual(bpe.add_X_years(32), 47)
+        initial_age = 15
+        added_age = 32
+        bpe = BazelPythonExample("John", initial_age)
+        bpe.print_age()
+        bpe.add_X_years(added_age)
+        self.assertEqual(bpe.age, initial_age+added_age)
+        bpe.print_age()
     
-    def test_isResultFloat(self):
-        bpe = BazelPythonExample(1, 22)
-        print "the result is not float"
-        self.assertIsInstance(bpe.age, float)
+    def test_isResultName(self):
+        bpe = BazelPythonExample("Martin", 11)
+        self.assertIsInstance(bpe.name, str)
+        bpe.print_name()
     
     def test_isResultInt(self):
-        bpe = BazelPythonExample(1, 22)
-        print "the result is int"
+        bpe = BazelPythonExample("Fido", 22)
         self.assertIsInstance(bpe.age, int)
+        bpe.print_age()
+
+if __name__ == '__main__':
+    unittest.main()
